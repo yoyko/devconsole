6b00c1429e51809ebc1dcacbf7417bfd1bb32534
Jozef Šiška <jozef.siska@legrand.us>
2017-07-31 22:39:44 +0200

    message browse edit: add from/count integer validation

--- src/Message/Edit/Browse.elm
+++ src/Message/Edit/Browse.elm
@@ -3,17 +3,29 @@ import Message.Edit.Model exposing (Model, Msg(..), Context)
 import Html exposing (Html, div)
 import Html.Attributes exposing (class)
 import Message.Edit.Parts as Parts exposing (textfield)
+import Material.Options as Options
+import Material.Textfield as Textfield
 import Json.Encode
+import Result.Extra
 
 browseEdit : Context msg -> (Context msg -> Model -> Html msg) -> Model -> Html msg
 browseEdit ctx send model =
-  div
-    [ class "browseEdit" ]
-    [ Parts.url ctx [2, 3, 0] model
-    , textfield ctx [2, 3, 1] "from" model.browseFrom BrowseFrom []
-    , textfield ctx [2, 3, 2] "count" model.browseCount BrowseCount []
-    , send ctx model
-    ]
+  let
+    isValidInt str =
+      (str |> String.toInt |> Result.Extra.isOk) || (String.isEmpty str)
+    validateInt str =
+      Textfield.error "Number!"
+        |> Options.when (not <| isValidInt str)
+  in
+    div
+      [ class "browseEdit" ]
+      [ Parts.url ctx [2, 3, 0] model
+      , textfield ctx [2, 3, 1] "from" model.browseFrom BrowseFrom
+          [ validateInt model.browseFrom ]
+      , textfield ctx [2, 3, 2] "count" model.browseCount BrowseCount
+          [ validateInt model.browseCount ]
+      , send ctx model
+      ]
 
 browseResult : Model -> Result String Json.Encode.Value
 browseResult model =
